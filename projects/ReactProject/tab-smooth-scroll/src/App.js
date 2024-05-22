3e132fdf63ff2766536e9df919ad29d93e9edd75
ultra <tt.soccer.022484@gmail.com>
2024-05-23 05:31:43 +0900

    /tabsで遷移するようルーティング

--- projects/ReactProject/tab-smooth-scroll/src/App.js
+++ projects/ReactProject/tab-smooth-scroll/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
+// import Tabs from './Tabs';
 import TabContainer from './components/TabContainer';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <TabContainer />
+  //   </div>
+  // );
   return (
     <div className="App">
-      <TabContainer />
+    <Router>
+      <Routes>
+        <Route path="/tabs" element={<TabContainer />} />
+        <Route path="/" element={<Navigate to="/tabs" />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
