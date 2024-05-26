c3555da8cf0bf941d99e22df16ed1e9817b0eb77
ultra <tt.soccer.022484@gmail.com>
2024-05-26 16:50:39 +0900

    useEffect 呼び出しタイミングの違い

--- projects/ec-app/src/component/Counter.jsx
+++ projects/ec-app/src/component/Counter.jsx
@@ -3,23 +3,37 @@ import React, { useEffect, useState } from 'react'
 const Counter = () => {
   const [count, setCount] = useState(0)
   const countUp = () => {
-    setCount(prevState => prevState +1)
+    setCount(prevState => prevState + 1)
   }
 
   const countDown = () => {
-    setCount(prevState => prevState -1)
+    setCount(prevState => prevState - 1)
   }
 
-  // useEffect 再描画（レンダリング）後に呼び出される
+  // 毎回実行される
   useEffect(() => {
     console.log("Current count is...", count)
-    });
+  });
+
+  // 初回レンダリング後のみ実行される
+  useEffect(() => {
+    console.log("ACurrent count is...", count)
+  }, []);
+  // triggerが変更されるたびに実行される
+  // useEffect(() => {
+  //   console.log("Current count is...", count)
+  // }, [trigger]);
+
+  // trigger1 or trigger2が変更されるたびに実行される
+  // useEffect(() => {
+  //   console.log("Current count is...", count)
+  // }, [trigger1, trigger2]);
 
   return (
     <div>
-    <p>現在のカウント数: {count}</p>
-    <button onClick={countUp}>up</button>
-    <button onClick={countDown}>down</button>
+      <p>現在のカウント数: {count}</p>
+      <button onClick={countUp}>up</button>
+      <button onClick={countDown}>down</button>
     </div>
 
   )
