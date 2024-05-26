46e17c65997e716af4f1b1d96196e5fee44a82a5
ultra <tt.soccer.022484@gmail.com>
2024-05-26 22:01:17 +0900

    useEffect API呼び出し

--- projects/ec-app/src/component/Counter.jsx
+++ projects/ec-app/src/component/Counter.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const countUp = () => {
     setCount(prevState => prevState + 1)
   }
@@ -14,6 +17,28 @@ const Counter = () => {
   useEffect(() => {
     console.log("Current count is...", count)
   });
+
+  // 初回レンダリング後のみ実行される
+  useEffect(() => {
+    // APIからデータをフェッチ
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setArticles(data); // APIから取得したデータを状態に保存
+        setLoading(false); // ローディング状態を解除
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error); // エラー状態を設定
+        setLoading(false); // ローディング状態を解除
+      });
+  }, []);
 
   // 初回レンダリング後のみ実行される
   useEffect(() => {
@@ -34,7 +59,25 @@ const Counter = () => {
       <p>現在のカウント数: {count}</p>
       <button onClick={countUp}>up</button>
       <button onClick={countDown}>down</button>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+
+      {!loading && !error && (
+        <div>
+          <h1>Articles</h1>
+          <ul>
+            {articles.map((article) => (
+              <li key={article.id}>
+                <h2>{article.title}</h2>
+                <p>{article.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
+
+
 
   )
 }
