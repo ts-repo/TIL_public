043d17cd3cf54a05a35a79daa5f72e79f87c01bf
ultra <tt.soccer.022484@gmail.com>
2025-11-02 00:18:45 +0900

    フォルダ配置間違えたので削除

--- Tips/docker_test/lambda-oracledb/build_layer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-docker build -t oracle-instantclient-layer .
-docker create --name oracle-layer-tmp oracle-instantclient-layer
-docker cp oracle-layer-tmp:/opt/oracle_instantclient_layer.zip ./oracle_instantclient_layer.zip
-docker rm oracle-layer-tmp
-
-echo "✅ oracle_instantclient_layer.zip を作成しました！"
